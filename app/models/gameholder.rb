042ff0a4b08794935042cf4059b36d80f3e5a9af
Allen Lee <allen866129@gmail.com>
2014-08-21 09:55:23 +0800

    modify new gameholder page

--- app/models/gameholder.rb
+++ app/models/gameholder.rb
@@ -1,4 +1,6 @@
 class Gameholder < ActiveRecord::Base
   attr_accessible :address, :city, :county, :court_name, :lat, :lng, :name, :phone, :sponsor, :user_id, :zipcode , :approved
   belongs_to :user
+  scope :waitingforapprove, where( :approved => false )
+  scope :alreadyapproved, where( :approved => true )
 end
